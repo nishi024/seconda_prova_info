44c9f20d9731bd5e53d380c0e54ab52636e208b5
nishi024 <92784019+nishi024@users.noreply.github.com>
2023-05-16 17:39:46 +0200

    Update index.php

--- Sito Aeroporto/index.php
+++ Sito Aeroporto/index.php
@@ -1,24 +1,29 @@
+<!DOCTYPE html>
 <html>
-    <head>
-    </head>
 
-    <body>
-        <div>
-            <center>
-                <h1>inserisci i tuoi dati qui.</h1>
-                <div class="form">
-                    <form action="home.php" method="get">
-                        <p>username</p>
-                        <input type="text" name="username" require>
-                        <p>Password</p>
-                        <input type="password" name="Pass" require>
-                        <br>
-                        <br>
-                        <input type="submit">
-                    </form>
-                    <p>se non sei registrato clicca <a href="registrazione.php">qui</a></p>
-                </div>
-            </center>
-        </div>
-    </body>
+<head>
+    <title>Pagina di Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Inserire le credenziali</h1>
+        <form action="home.php" method="get">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Inserisci il tuo username" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" class="form-control" id="pass" name="pass" placeholder="Inserisci la tua password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Accedi</button>
+        </form>
+        <p>Se non sei registrato clicca <a href="registrazione.php">qui</a></p>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+
 </html>
