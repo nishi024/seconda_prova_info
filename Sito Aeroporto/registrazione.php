9b6c0303594b88c8c251691eab63cbbf73b9f9e0
nishi024 <92784019+nishi024@users.noreply.github.com>
2023-05-17 08:59:50 +0200

    Update registrazione.php

--- Sito Aeroporto/registrazione.php
+++ Sito Aeroporto/registrazione.php
@@ -11,21 +11,55 @@ session_start();
 </head>
 
 <body>
-    <div class="container w-25 mt-3 p-3 border border-primary rounded">
+    <div class="container w-25 mt-3 p-3 border border-info rounded">
         <h1 class="mb-3 text-center">Registrati</h1>
         <form action="index.php" method="post">
-            Cognome: <input type="text" name="cognome" id="cognome" class="form-control" required><br>
+            Documento: <input type="text" name="documento" id="documento" class="form-control" placeholder="Inserire il documento" required><br>
+            Nome: <input type="text" name="nome" id="nome" class="form-control" placeholder="Inserire nome" required><br>
+            Cognome: <input type="text" name="cognome" id="cognome" class="form-control" placeholder="Inserire cognome" required><br>
+            <div class="form-group">
+                <label for="nazionalita">Nazionalità:</label>
+                <input type="text" class="form-control" id="nazionalita" name="nazionalita" placeholder="Inserisci la nazionalità" required>
+            </div><br>
+            <div class="form-group">
+                <label for="aeroportoDest">Aeroporto di destinazione:</label>
+                <input type="text" class="form-control" id="aeroportoDest" name="aeroportoDest" placeholder="Inserisci l'aeroporto di destinazione">
+            </div><br>
+            <div class="form-group">
+                <label for="aeroportoProv">Aeroporto di provenienza:</label>
+                <input type="text" class="form-control" id="aeroportoProv" name="aeroportoProv" placeholder="Inserisci l'aeroporto di provenienza">
+            </div><br>
+            <div class="form-group">
+                <label for="stato">Stato:</label>
+                <input type="text" class="form-control" id="stato" name="stato" placeholder="Inserisci lo stato">
+            </div><br>
+            <div class="form-group">
+                <label for="motivoViaggio">Motivo del viaggio:</label>
+                <input type="text" class="form-control" id="motivoViaggio" name="motivoViaggio" placeholder="Inserisci il motivo del viaggio" required>
+            </div><br>
 
-            Nome: <input type="text" name="nome" id="nome" class="form-control" required><br>
-
-            Username: <input type="text" class="form-control" id="username" name="username" placeholder="Inserire username" required><br>
-            Password: <input type="password" name="password" id="password" class="form-control" placeholder="Inserire password" required> <br>
-            <center>
-                <a href="index.php"><button type="button" class="btn btn-secondary">Annulla</button>
-                    <input type="submit" name="register" value="conferma" class="btn btn-danger">
-            </center>
+            <div class="form-group">
+                <label for="categoria">Categoria:</label>
+                <input type="text" class="form-control" id="categoria" name="categoria" placeholder="Inserisci la categoria" required>
+            </div>
+            <div class="form-group">
+                <label for="descrizione">Descrizione:</label>
+                <input type="text" class="form-control" id="descrizione" name="descrizione" placeholder="Inserisci la descrizione" required>
+            </div>
+            <div class="form-group">
+                <label for="quantita">Quantità:</label>
+                <input type="number" class="form-control" id="quantita" name="quantita" placeholder="Inserisci la quantità" required>
+            </div>
+            <div class="form-group">
+                <label for="stato">Stato:</label>
+                <input type="text" class="form-control" id="stato" name="stato" placeholder="Inserisci lo stato" required>
+                <br><center>
+                    <a href="index.php"><button type="button" class="btn btn-secondary">Annulla</button>
+                        <input type="submit" name="register" value="Conferma" class="btn btn-info">
+                </center>
         </form>
     </div>
+
 </body>
 
 </html>
