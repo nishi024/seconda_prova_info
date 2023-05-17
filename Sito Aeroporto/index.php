33b7fe37d882ee07b1833cc7f5d1599d3128b96d
nishi024 <92784019+nishi024@users.noreply.github.com>
2023-05-17 09:00:18 +0200

    Update index.php

--- Sito Aeroporto/index.php
+++ Sito Aeroporto/index.php
@@ -2,28 +2,44 @@
 <html>
 
 <head>
-    <title>Pagina di Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Pagina login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 
 <body>
     <div class="container">
-        <h1>Inserire le credenziali</h1>
-        <form action="home.php" method="get">
-            <div class="form-group">
-                <label for="username">Username:</label>
-                <input type="text" class="form-control" id="username" name="username" placeholder="Inserisci il tuo username" required>
+        <div class="row justify-content-center" class="form-group">
+            <div class="col-md-6">
+                <h2 class="text-center">Inserire credenziali</h2>
+                <form action="home.php" method="get">
+                    <div class="form-group">
+                        <label for="username">Username:</label>
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Inserisci il tuo username" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input type="password" class="form-control" id="pass" name="pass" placeholder="Inserisci la tua password" required>
+                    </div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <button type="submit" class="btn btn-info">Accedi</button>
+                            </div>
+                            <div class="col-md-6">
+                                <a href="registrazione.php" class="btn btn-link">Registrati</a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
             </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" class="form-control" id="pass" name="pass" placeholder="Inserisci la tua password" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Accedi</button>
-        </form>
-        <p>Se non sei registrato clicca <a href="registrazione.php">qui</a></p>
+
+        </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
