dd5383b090adead9fae26823afeb5a09de0ce3bf
DanielPacc <98812974+DanielPacc@users.noreply.github.com>
2023-05-17 10:27:54 +0200

    Update home.php

--- Sito Aeroporto/home.php
+++ Sito Aeroporto/home.php
@@ -10,20 +10,21 @@
 
     if(!empty($_GET)){
         $username=$_GET["username"];
-        $pass=$_GET["Pass"];
+        $pass=$_GET["pass"];
         $a=0;
 
 
-        $query = "SELECT d.pass FROM dati_accesso as d where d.username='$username' and pass='$pass';";
+        $query = "SELECT d.username FROM controllore as d where d.username='$username' and pass='$pass';";
         $result = $conn->query($query)
-        or die("<p>meglio che cambi lavoro</p> <br>");
+        or die("<p>meglio cambi lavoro</p> <br>");
         
         while($row=$result->fetch_array()){
-                echo "<center><h1>ti sei loggato correttamente</h1><br>";
-    
-                echo "<p>" . $row['pass'] . $pass;
+            echo "<center><h1>ti sei loggato correttamente</h1><br>";
+            if($row["username"]=="admin"){echo "<center><p>vai alla pagina di aggiunta controlli " . "<a href=" . "admin.php" . ">qui</a>.<br></center>";}
+            else{
+                
                 echo "vai alla pagina di aggiunta controlli " . "<a href=" . "aggiungiCC.php" . ">qui</a> oppure vai alla pagina per vedere i controlli <a href=" . "controlli.php" . ">qui</a></p> <br></center>";
-    
+            }
                 $a=1;
                 $_SESSION["user"]=$username;
                 
